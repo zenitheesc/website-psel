bd81ef7228a479f96e3ea6038f83d3e6fa538453
itMatos <italomatos764@gmail.com>
2026-02-16 00:41:09 -0300

    Update Wires component formatting and fix password generation constants

--- passwords/passwords.ts
+++ passwords/passwords.ts
@@ -1,10 +1,10 @@
-const currentDate = new Date().getFullYear(); // 2025
-const currentDateString = [...currentDate.toString()]; // "2025"
+const currentDate = new Date().getFullYear(); // 2026, 2027, 2028, 2029, 2030.....
 
-export const paraComecar = `PSEL_${currentDateString}` as const;
-export const numerosBinarios = `#Z3NS3NH4_${currentDateString}#` as const;
+export const paraComecar = `PSEL_${currentDate}` as const;
+export const numerosBinarios = `#Z3NS3NH4_${currentDate}#` as const;
 export const sequenciaComandosHover = "3-4-4-3-4-2-4-3-4-2-2-2-1-1" as const;
 export const sequenciaSimbolos = "2-1-3-4" as const;
+export const fioParaDesconectar = "2" as const;
 export const fiosColoridos = {
   modulo_um: "2-1-3",
   modulo_dois: "3-2-2-4",
@@ -16,5 +16,6 @@ export const passwords = {
   numerosBinarios,
   sequenciaComandosHover,
   sequenciaSimbolos,
+  fioParaDesconectar,
   fiosColoridos,
 };
