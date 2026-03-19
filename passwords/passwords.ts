7f2cde9196d185f0def68adcad7d67c60b0ebc99
itMatos <italomatos764@gmail.com>
2026-03-18 21:07:46 -0300

    Fix formatting

--- passwords/passwords.ts
+++ passwords/passwords.ts
@@ -1,6 +1,6 @@
 const currentDate = new Date().getFullYear(); // 2026, 2027, 2028, 2029, 2030.....
 
-export const paraComecar = `PSEL_${currentDate}` as const;
+export const paraComecar = `#PSEL_${currentDate}#` as const;
 export const numerosBinarios = `#Z3NS3NH4_${currentDate}#` as const;
 export const sequenciaComandosHover = "3-4-4-3-4-2-4-3-4-2-2-2-1-1" as const;
 export const sequenciaSimbolos = "2-1-3-4" as const;
